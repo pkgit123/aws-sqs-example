8295a59495cb0284d42cc2773f7431c3238bd822
Peter Kim <petekim@gmail.com>
2020-11-04 11:40:10 -0800

    Update aws-lambda-sqs-eventsource.py

--- aws-lambda-sqs-eventsource.py
+++ aws-lambda-sqs-eventsource.py
@@ -9,8 +9,20 @@ def lambda_handler(event, context):
     '''
     Use SQS event source, print to console.
     
-    The SQS event json has key 'Records', where value is list with single item.
-    The single item is an embedded json with key 'body'; the value is str message.
+    The SQS event json has key 'Records', where value is list with a single item.
+    
+    The single item is an embedded json with keys
+     * 'body': the value is str message
+     * 'messageAttributes': the value is json/dict
+            - Keys are messageAttribute names
+                - Values are embedded json/dict
+                    - 'stringValue' -> string with message attribute value
+                    - 'dataType' -> string where possible values are: 'String', 'Number'
+                    - stringListValues' -> empty list, according to AWS documentation reserved for future use
+                    - 'binaryListValues' -> empty list, according to AWS documentation reserved for future use
+                    
+    Reference:
+     * # https://docs.aws.amazon.com/cli/latest/reference/sqs/send-message.html
     '''
     
     # Event Source: SQS message body
